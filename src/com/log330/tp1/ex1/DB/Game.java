d47de8cf0a13aa38480e95eddee938134b285e81
DonPellegrino <hyssar@hotmail.com>
2012-10-04 19:30:35 -0400

    Mise à jour des tests

--- src/com/log330/tp1/ex1/DB/Game.java
+++ src/com/log330/tp1/ex1/DB/Game.java
@@ -83,6 +83,7 @@ public class Game {
 			}
 		}
 		
+		
 		return ret;
 	}
 }
--- src/com/log330/tp1/ex1/DB/test.java
+++ src/com/log330/tp1/ex1/DB/test.java
@@ -4,17 +4,17 @@ public class test {
 
 	public static void main(String[] args) {
 		
-		DB.getInstance().setPlayer(0, 10, "A A", 1);
-		DB.getInstance().setPlayer(1, 20, "B B", 1);
-		DB.getInstance().setPlayer(2, 30, "C C", 1);
-		DB.getInstance().setPlayer(5, 60, "D D", 2);
-		DB.getInstance().setPlayer(6, 70, "E E", 2);
-		DB.getInstance().setPlayer(7, 80, "F F", 2);
+		DB.getInstance().setPlayer(0, 10, "A A", DB.TEAMRED);
+		DB.getInstance().setPlayer(1, 20, "B B", DB.TEAMRED);
+		DB.getInstance().setPlayer(2, 30, "C C", DB.TEAMRED);
+		DB.getInstance().setPlayer(5, 60, "D D", DB.TEAMBLUE);
+		DB.getInstance().setPlayer(6, 70, "E E", DB.TEAMBLUE);
+		DB.getInstance().setPlayer(7, 80, "F F", DB.TEAMBLUE);
 		System.out.println(DB.getInstance().getAllPlayers().toString());
 		
 		DB.getInstance().newGame();
-		DB.getInstance().addGoal(DB.getInstance().getPlayerByNum(10, 1), null, 5);
-		DB.getInstance().addGoal(DB.getInstance().getPlayerByNum(20, 1), null, 15);
+		DB.getInstance().addGoal(DB.getInstance().getPlayerByNum(10, DB.TEAMRED), null, 5);
+		DB.getInstance().addGoal(DB.getInstance().getPlayerByNum(20, DB.TEAMRED), null, 15);
 		System.out.println(DB.getInstance().getScore().toString());
 		
 		System.out.println(DB.getInstance().getPeriod());
@@ -29,6 +29,8 @@ public class test {
 		
 		DB.getInstance().newGame();
 		System.out.println(DB.getInstance().getGoals().toString());
+		
+		System.out.println(DB.getInstance().getTop());
 	}
 
 }
